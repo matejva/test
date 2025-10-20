5ab4030b00561ad9c0a0d3396f04ab3c88e14d46
matejva <57626975+matejva@users.noreply.github.com>
2025-10-20 19:06:19 +0200

    Create upgrade.py

---
+++ upgrade.py
@@ -0,0 +1,12 @@
+from app import db
+from sqlalchemy import text
+
+print("🚀 Spúšťam úpravu databázy...")
+
+try:
+    with db.engine.connect() as conn:
+        conn.execute(text('ALTER TABLE users ALTER COLUMN password TYPE VARCHAR(200);'))
+        conn.commit()
+    print("✅ Stĺpec 'password' bol upravený na VARCHAR(200).")
+except Exception as e:
+    print(f"❌ Chyba počas úpravy databázy: {e}")
