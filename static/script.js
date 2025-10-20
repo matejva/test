e84dcef88405dda75b1c1ae11abed514c09e04c4
matejva <57626975+matejva@users.noreply.github.com>
2025-10-20 12:52:18 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ function openEditModal(id, projectId, amount, note) {
   const modal = document.getElementById('editModal');
   modal.style.display = 'block';
   const form = document.getElementById('editForm');
-  form.action = '/edit_record/' + id;
+  form.action = '/edit_record/' + id;  // iba ak route existuje
   form.project_id.value = projectId;
   form.amount.value = amount;
   form.note.value = note;
